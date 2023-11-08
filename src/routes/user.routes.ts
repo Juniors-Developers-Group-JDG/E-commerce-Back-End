05f9af99fd8c09045b1f6305fb5b801c3cad0957
Guilherme <guisou1902@gmail.com>
2023-11-08 17:54:07 -0300

    feat: added a register route for users

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { UserController } from '../controllers/user.controller'
+// import { upload } from '../middleware/uploadImage'
+
+const userRouter = Router()
+
+const userController = new UserController()
+
+userRouter.post('/register/', userController.register)
+
+export { userRouter }
